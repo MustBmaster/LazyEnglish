4916bdc4468b21a31fcbdc4a86c80bb760b809e9
MustBMaster <minhmocprovdqq@gmail.com>
2022-03-15 01:15:28 +0700

    sửa code HW6

--- HomeWork6/HW6.js
+++ HomeWork6/HW6.js
@@ -1,8 +1,50 @@
-var ten = prompt("nhap ten");
-if(ten == 'a'){
-    console.log('NY')
-}else if(ten == 'b'){
-    console.log("0Ny")
-}else{
-    console.log("chịu")
+// bài 1
+//a
+function Bai1A(){
+    for(var i=100;i>0;i--){
+        console.log(i)
+    }
 }
+//b
+function Bai1B(n){
+    let sum=0;
+    if(n<=2) return 0;
+    else{
+        for(let i=1;i<=n;i++){
+            sum+=n/(n*(n+1));
+        }
+    }
+}
+
+// bài 2
+function Bai2(n){
+    console.log("In bảng nhân")
+    if(n<1||n>10){
+        console.log("Giá trị không hợp lệ")
+    }else{
+        for(var i=1;i<=10;i++){
+            console.log(n+" * "+i+" = "+n*i)
+        }
+    }
+}
+
+// bài 3
+function Bai3(m,n){
+    let Line="";
+    for(let i=0;i<m;i++){
+        Line+="*"
+    }
+    for(let i=0;i<n;i++){
+        console.log(Line)
+    }
+}
+
+// bài 4
+function Bai4(m){
+    let Line="";
+    for(let i=0;i<m;i++){
+        Line+="*"
+        console.log(Line)
+    }
+}
+
